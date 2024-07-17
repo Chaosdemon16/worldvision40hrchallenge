f3df470394cd36071de62d60730805813c77b552
Oliver Ren <128006873+RoboticOllie@users.noreply.github.com>
2024-07-17 19:42:22 +1200

    added search functionality

--- home.js
+++ home.js
@@ -1,46 +1,69 @@
-function secret(key) {
+//Standard:
+//{
+//   name: "cause",
+//   img: "cause.png",
+//   src: "cause/cause.html",
+// },
+const causes = [
+  {
+    name: "A1",
+    img: "images/test.png",
+    src: "cause/index.html",
+  },
+  {
+    name: "A2",
+    img: "images/test.png",
+    src: "cause/index.html",
+  },
+  {
+    name: "A3",
+    img: "images/test.png",
+    src: "cause/index.html",
+  },
+  {
+    name: "B1",
+    img: "images/test.png",
+    src: "cause/index.html",
+  },
+  {
+    name: "B2",
+    img: "images/test.png",
+    src: "cause/index.html",
+  },
+  {
+    name: "B3",
+    img: "images/test.png",
+    src: "cause/index.html",
+  }
+]
+
+console.log(causes.length - 1)
+function display(info){
+  let cardsGrid = ""
+  for(let i = 0; i < info.length; i++){
+    cardsGrid += "<div class='card'><img class='cardPhoto' src='" + info[i].img + "'><b><p class='causeTitle'>" + info[i].name + "</p></b></div>"
+  }
+  document.querySelector("#cardsGrid").innerHTML = cardsGrid
+}
+display(causes)
+
+function search(key) {
   if (key.key === 'Enter') {
     if (document.querySelector('.searchBar').value === 'dvd') {
       document.querySelector('.js-secret-button')
         .innerHTML = '<button class="secret-button" onclick="magic()">takemetothemagic</button>';
-    } else if (document.querySelector('.searchBar').value === '') {
-      document.querySelector('.js-secret-button')
-        .innerHTML = '';
+    } else {
+      let clean = document.querySelector('.searchBar').value.replace(/[^a-zA-Z0-9]/g,'').toLowerCase()
+      let results = causes.filter(e => e.name.replace(/[^a-zA-Z0-9]/g,'').toLowerCase().includes(clean))
+      console.log(results)
+      display(results)
     }
 
   };
 };
-const causeNames = ['womens rights', 'cyber awareness', 'cyber security'];
-const finalCauses = ['Womens Rights', 'Cyber Awareness', 'Cyber Security'];
-const imgNames = ['womensrights', 'cyberawareness', 'cybersecurity'];
-
-function search(key) {
-  if (key.key === 'Enter') {
-    console.log(key.key);
-    let results = 0;
-    let loops = 0;
-    let html = '';
-    let input = document.querySelector('.searchBar').value;
-    input = input.toLowerCase();
-    causeNames.forEach((value, index) => {
-      if (input === causeNames[loops]) {
-        console.log(input);
-        results = results + 1;
-        html = `<div class="card">
-        <img class="cardPhoto" src="Photos/${imgNames[loops]}.png">
-        <h1 class="causeTitle">${finalCauses[loops]}</h1>
-      </div>`;
-      }
-      loops++;
-    });
-
-    if (html === '') {
-      document.querySelector('.main').innerHTML = `<h1>Sorry, ${input} isn't one of our 40 causes yet</h1>`;
-    } else {
-      document.querySelector('.cardsGrid').innerHTML = html;
-    }
-  }
-}
+const causeNames = ["womens rights", "cyber awareness", "cyber security"];
+const finalCauses = ["Women's Rights", "Cyber Awareness", "Cyber Security"];
+const imgNames = ["womensrights", "cyberawareness", "cybersecurity"];
 
 function magic() {
   document.location.href = "Magic/index.html";
